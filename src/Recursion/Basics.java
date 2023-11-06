e32af99867e035326d0520e73ffc200973fb9f1c
Harshan <suweeharshan@gmail.com>
2023-11-06 09:13:08 +0530

    binary search added to recursion

--- src/Recursion/Basics.java
+++ src/Recursion/Basics.java
@@ -6,11 +6,29 @@ public class Basics {
     public static void main(String[] args) {
 //       int value = print_n_numbers(4);
 //        System.out.println(value);
-        int[] arr = {1,2,2,1};
+        int[] arr = {1,2,2,5,7,8,10};
         System.out.println(Arrays.toString(arr));
-        System.out.println(palindrome(arr, 0, arr.length - 1));
-
+//        System.out.println(palindrome(arr, 0, arr.length - 1));
+        int idx = binary_search(arr,0,arr.length, 8);
+        System.out.println(idx);
     }
+
+    private static int binary_search(int[] arr, int start, int end, int target) {
+        if (start > end){
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (target == arr[mid]){
+            return mid;
+        }
+        else if (target > arr[mid]){
+            return binary_search(arr, mid+1, end, target);
+        }
+        else{
+            return binary_search(arr, start, mid-1, target);
+        }
+    }
+
     static int print_n_numbers(int n){
         if (n<=0) {
             return 1;
